6ae3360b4328fa048d4f968d8d810823ad816d5f
khalil khan <khalilkhankk3272711@gmail.com>
2024-08-29 09:21:35 +0500

    adding test middleware

--- index.js
+++ index.js
@@ -6,15 +6,38 @@ const authRoutes = require("./routes/AuthRoutes");
 const TaskRoutes = require("./routes/TaskRoutes");
 
 const app = express();
-app.use(express.json());
 
+// Middleware
+app.use(express.json());
 app.use(cors());
 
+// Logging Middleware
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
+// Database Connection
 ConnectDB();
 
+// Routes
 app.use("/", authRoutes);
 app.use("/", TaskRoutes);
 
+// Test Route
+app.get("/test", (req, res) => {
+  console.log("Test endpoint hit");
+  res.send("Test endpoint working");
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
